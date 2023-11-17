0b659e367064f3982d9bffbcafcafd107289c6ea
Sagar <sagarkph@gmail.com>
2023-11-17 15:37:31 +0500

    complete authentication

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -14,9 +15,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
-// const CONNECTION_URL =
-//     "mongodb+srv://admin:admin123@cluster0.5bhab64.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
